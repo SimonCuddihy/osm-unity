b09cfffafcd0207b85f64aa6a5ec8b2d26f11841
Simon Cuddihy <simoncuddihy@gmail.com>
2019-12-04 16:50:07 +0000

    Initial OSM Commit

---
+++ Assets/Scripts/MapReader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapReader : MonoBehaviour
+{
+
+    [Tooltip("The resource file that contains the OSM map data")]
+    public string resourceFile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var txtAsset = Resource.Load<TextAsset>(resourceFile);
+
+        XmlDocument doc = new XmlDocument();
+        doc.LoadXml(txtAsset.text);
+
+        SetBounds(doc.SelectSingleNode("/osm/bounds"));
+        GetNodes(doc.SelectNodes("/osm/node"));
+        GetWays(doc.SelectNodes("osm/way"));
+        
+
+    }
+}
+ 
